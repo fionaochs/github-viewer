7b44837b96134c45946a2a89f61d047fdd5acdbc
Fiona Ochs <fiona.oaks@gmail.com>
2020-04-28 16:45:20 -0700

    repo

--- src/components/UserControls/UserControls.jsx
+++ src/components/UserControls/UserControls.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const UserControls = ({ userName }) => (
-  <form onSubmit={this.props.handleSearch}>
+const UserControls = ({ userName, onSubmit }) => (
+  <>
     <input name="name" type="text" value={userName} />
-    <button name="button" type="button" onClick={this.props.onSubmit}>Search for user</button>
-  </form>
-
+    <button name="button" type="button" onClick={onSubmit}>Search for user</button>
+  </>
 );
 
 UserControls.propTypes = {
-  userName: PropTypes.string.isRequired
+  userName: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default UserControls;
--- src/containers/User/User.jsx
+++ src/containers/User/User.jsx
@@ -3,30 +3,37 @@ import { fetchUser } from '../../services/githubUser.js';
 import { fetchRepos } from '../../services/githubRepos.js';
 import UserControls from '../../components/UserDisplay/UserDisplay.jsx';
 import Repos from '../../components/UserRepos/Repos.jsx';
+import UserDisplay from '../../components/UserDisplay/UserDisplay.jsx';
 
 export default class User extends Component {
   state = {
-    userName: '',
-    repos: []
+    userName: 'fionaochs',
+    userDisplay: {},
+    repos: [],
+    loading: true
   }
-
+  
   componentDidMount() {
-  }
-  
-  handleSubmit = () => {
-    fetchUser()
-      .then(userName => this.setState({ userName }));
-  
-    fetchRepos()
+    fetchUser(this.state.userName)
+      .then(userDisplay => this.setState({ userDisplay, loading: false }));
+
+    fetchRepos(this.state.userName)
       .then(repos => this.setState({ repos }));
   }
 
-  render() {
-    const { userName, repos } = this.state;
+  handleSubmit = ({ target }) => { 
+    this.setState({ [target.name]: target.value });
+  };
 
+  render() {
+    const { userName, repos, userDisplay, loading } = this.state;
+
+    if(loading) return <h1>Loading</h1>;
+    
     return (
       <>
         <UserControls onSubmit={this.handleSubmit} userName={userName} />
+        <UserDisplay {...userDisplay}/>
         <Repos repos={repos} />
       </>
     );
